94f6f63c2716483f01116fe2eeacf1d932fdbd7e
HyunhoChoi <hyunh317@naver.com>
2020-08-23 13:25:10 +0900

    0823 event banner insert

--- project/mainapp/views.py
+++ project/mainapp/views.py
@@ -5,4 +5,11 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    ediya_eventpage = 'https://www.ediya.com/contents/event.html?tb_name=event&bbs_section=view&Ctg=&page=1&idx=122/'
+    angelinus_eventpage = 'http://www.angelinus.com/Event/Event_View.asp?Mode=VIEW&EventType=Event&Idx=626&SearchEventGubun=0/'
+    pascucci_eventpage = 'http://www.caffe-pascucci.co.kr/event/eventView.asp?teSeq=431/'
+
+    context = {'ediya_eventpage': ediya_eventpage,
+               'angelinus_eventpage': angelinus_eventpage,
+               'pascucci_eventpage': pascucci_eventpage}
+    return render(request,'home.html',context)
